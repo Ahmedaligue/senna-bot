91abc302db1279f4090604b36d0658ca666f3b82
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-24 16:41:22 +0300

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,146 +1,21 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-import uploadFile from '../lib/uploadFile.js'
+import fetch from 'node-fetch'
 
-import fetch from 'node-fetch';
-export async function before(m, { conn, text, participants }) {
-   let users = participants.map(u => conn.decodeJid(u.id))
-   let q = m.quoted ? m.quoted : m || m.text || m.sender
-   let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-   let messageType = m.quoted ? q.mtype : 'extendedTextMessage'
-   let messageContent = m.quoted ? c.message[q.mtype] ?? {} : { text: '' || c }
-   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; // تعريف المتغير who
+let handler = m => m
+handler.all = async function (m) {
+	
+	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/c6e93e154336db7585c98.jpg')
+	
+	//reply link wa
+   global.rpl = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'فيديو', description: 'دعم الجروب', title: packname, body: 'دعم الجروب', thumbnailUrl: pp, sourceUrl: dygp }}} 
+	
+	//reply link PayPal
+    global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'فيديو', description: 'يتبرع', title: 'يوتيوب', body: 'إبقاء الروبوت يعمل ', thumbnailUrl: pp, sourceUrl: fgyt }}}
+	
+	//reply link yt
+    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'الفيديو', description: 'اشتراك : ' + fgyt, title: 'YouTube', body: 'تعلم كيفية إنشاء الروبوتات الخاصة بك', thumbnailUrl: pp, sourceUrl: fgyt }}}
 
-   if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات`; // فحص ما إذا كان المستخدم موجودًا في قاعدة البيانات
+	global.fcon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `ZORO-BOT`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'GURU-BOT'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
-   let { name } = global.db.data.users[who]; // تعريف المتغير name من قاعدة البيانات
-   
-   global.fcontact = {
-        key: {
-            fromMe: false,
-            participant: `0@s.whatsapp.net`,
-            remoteJid: 'status@broadcast'
-        },
-        message: {
-            contactMessage: {
-                displayName: `${name}`,
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        }
-    }
-   
-   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg');
-
-  let nam = "✨  𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕  ✨"
-   
-  // Respuesta con enlace de WhatsApp
-  global.rpl = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: bgp,
-        mediaType: 'VIDEO',
-        description: 'support group',
-        title: packname,
-        body: 'grupo de soporte',
-        thumbnailUrl: pp,
-        sourceUrl: bgp
-      }
-    } 
-  };
-
-  // Respuesta con enlace de Canal de WhatsApp
-  global.rcanal = {
-    contextInfo: { 
-    mentionedJid: users,
-       isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-    }
-  }, { quoted: global.fcontact }
-    // Forwarded
-  global.fwc = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }
-    } 
-  };
-
-  // Respuesta con enlace de PayPal
-  global.rpyp = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: fgpyp,
-        mediaType: 'VIDEO',
-        description: 'Donate',
-        title: 'PayPal',
-        body: 'ayuda a mantener el bot activo',
-        thumbnailUrl: pp,
-        sourceUrl: fgpyp
-      }
-    }
-  };
-
-  // Respuesta con enlace de Instagram
-  global.rpig = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: fgig,
-        mediaType: 'VIDEO',
-        description: 'Sigueme por Instagram',
-        title: 'Instagram',
-        body: 'sigueme por Instagram',
-        thumbnailUrl: pp,
-        sourceUrl: fgig
-      }
-    }
-  };
-
-  // Respuesta con enlace de YouTube
-  global.rpyt = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        showAdAttribution: true,
-        mediaUrl: fgyt,
-        mediaType: 'VIDEO',
-        description: 'Suscribete: ' + fgyt,
-        title: 'FG YouTube',
-        body: 'aprende a crear tus propios bots',
-        thumbnailUrl: pp,
-        sourceUrl: fgyt
-      }
-    }
-  }
-  
-  
-  
-}
+} 
+export default handler
